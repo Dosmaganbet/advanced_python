e35a836129c75088d420173cb17d8eda34a41d13
Doskhan <doskhandosm@gmail.com>
2025-12-25 19:10:13 +0500

    updat

--- week-2/task6.py
+++ week-2/task6.py
@@ -3,3 +3,8 @@ def all_eq(lst):
         return []
     m = max(len(s) for s in lst)
     return [s + "_" * (m - len(s)) for s in lst]
+
+line = input()
+words = line.split()
+
+print(all_eq(words))
